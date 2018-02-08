fe3d1ac68e5f727b980ce39cde61f34a09108f82
Teacher <shaojung@gmail.com>
2018-02-08 10:58:33 +0800

    Service countdown

--- app/src/main/java/tw/com/pcschool/dd2018020801/MyService.java
+++ app/src/main/java/tw/com/pcschool/dd2018020801/MyService.java
@@ -1,11 +1,20 @@
 package tw.com.pcschool.dd2018020801;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
+    NotificationChannel channelLove;
+    String idLove = "LOVE";
+    NotificationManager nm;
+    int NOTIFICATION_ID = 879;
     public MyService() {
     }
 
@@ -13,6 +22,7 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d("MyService", "This is onCreate");
+        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     }
 
     @Override
@@ -23,7 +33,7 @@ public class MyService extends Service {
             public void run() {
                 super.run();
                 int i;
-                for (i=0;i<20;i++)
+                for (i=0;i<10;i++)
                 {
                     try {
                         Thread.sleep(1000);
@@ -32,6 +42,8 @@ public class MyService extends Service {
                         e.printStackTrace();
                     }
                 }
+
+                sentNotification();
             }
         }.start();
 
@@ -39,6 +51,56 @@ public class MyService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
+    @TargetApi(Build.VERSION_CODES.O)
+    @SuppressWarnings("deprecation")
+    private void sentNotification()
+    {
+        Notification.Builder builder;
+        if (Build.VERSION.SDK_INT >= 26)
+        {
+            builder = new Notification.Builder(MyService.this, idLove);
+        }
+        else
+        {
+            builder = new Notification.Builder(MyService.this);
+        }
+
+        // Intent it = new Intent(MainActivity.this, InfoActivity.class);
+        // it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP |
+        //         Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        // PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 123, it,
+        //         PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentTitle("測試");
+        builder.setContentText("這是內容");
+        if (Build.VERSION.SDK_INT >= 26)
+        {
+            builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        }
+        else
+        {
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+        }
+        builder.setAutoCancel(true);
+        // builder.setContentIntent(pi);
+
+        Notification notify = builder.build();
+        nm.notify(NOTIFICATION_ID, notify);
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public void regChannel()
+    {
+        channelLove = new NotificationChannel(
+                idLove,
+                "Channel Love",
+                NotificationManager.IMPORTANCE_HIGH);
+        channelLove.setDescription("最重要的人");
+        channelLove.enableLights(true);
+        channelLove.enableVibration(true);
+
+        nm.createNotificationChannel(channelLove);
+    }
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
