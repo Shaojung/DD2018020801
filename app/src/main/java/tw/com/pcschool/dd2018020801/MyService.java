02269bc339678b9cb98c68bd486840fc189b0627
Teacher <shaojung@gmail.com>
2018-02-08 10:14:40 +0800

    Service countdown

--- app/src/main/java/tw/com/pcschool/dd2018020801/MyService.java
+++ app/src/main/java/tw/com/pcschool/dd2018020801/MyService.java
@@ -18,6 +18,24 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("MyService", "This is onStartCommand");
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                int i;
+                for (i=0;i<20;i++)
+                {
+                    try {
+                        Thread.sleep(1000);
+                        Log.d("MyService", "Delay, i=" + i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+
+
         return super.onStartCommand(intent, flags, startId);
     }
 
